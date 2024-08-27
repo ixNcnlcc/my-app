67b329c15a06f80bad9465288ee1e7e46e998691
Nutchanon <s65122519065@ssru.ac.th>
2024-08-27 11:09:16 +0700

    toggle image and word

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 function App() {
-  const [image, setImage] = useState('https://upload.wikimedia.org/wikipedia/commons/e/ec/Persian_Cat_7_UK.jpg');  // Initialize state with the word 'Hello'
-
+  const [image, setImage] = useState('https://upload.wikimedia.org/wikipedia/commons/e/ec/Persian_Cat_7_UK.jpg'); 
+  const [caption, setCaption] = useState('Cat');  // Initialize state with the word 'Cat'
   const toggleImage = () => {
     // Use an if statement to toggle between the words 'Hello' and 'Goodbye'
     if (image === 'https://upload.wikimedia.org/wikipedia/commons/e/ec/Persian_Cat_7_UK.jpg') {
       setImage('https://upload.wikimedia.org/wikipedia/commons/1/18/Dog_Breeds.jpg');
+      setCaption('Dog');
     } else {
       setImage('https://upload.wikimedia.org/wikipedia/commons/e/ec/Persian_Cat_7_UK.jpg');
+      setCaption('Cat');
     }
   };
 
@@ -15,6 +17,8 @@ function App() {
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       {/* Display the current image */}
       <img src={image} alt="Toggle" style={{ width: '50%', maxWidth: '500px' }} />
+      {/* Display the current caption */}
+    <p>{caption}</p>
       {/* Button to toggle the image */}
       <div>
         <button onClick={toggleImage} style={{ marginTop: '20px' }}>Toggle Image</button>
